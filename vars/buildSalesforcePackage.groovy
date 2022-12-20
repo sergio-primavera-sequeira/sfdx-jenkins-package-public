85cc512afe3765c385244271139d057adbf2f91c
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-20 14:30:52 -0500

    Update buildSalesforcePackage.groovy

--- vars/buildSalesforcePackage.groovy
+++ vars/buildSalesforcePackage.groovy
@@ -13,6 +13,8 @@ def call(String dev_hub = 'none') {
 	
 	try{
 		withCredentials([file(credentialsId: SFDC_ORG_01_JWT_KEY_CRED_ID, variable: 'jwt_key_file')]) {
+			def result = sfdx.command("sfdx --version")
+			echo "${result}"
 			return '!!!TEST SPS!!!'
 		}
 	} catch(Exception e) {
