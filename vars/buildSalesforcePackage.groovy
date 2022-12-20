341c7ed6516d4f8803d298baab7dbc07643b6377
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-20 10:52:07 -0500

    Update buildSalesforcePackage.groovy

--- vars/buildSalesforcePackage.groovy
+++ vars/buildSalesforcePackage.groovy
@@ -1,3 +1,17 @@
+#!/usr/bin/env groovy
+import java.util.Date
+import java.text.SimpleDateFormat
+
+def call(String DEV_HUB = 'none') {
+}
+
+def configJob(){
+	echo 'Starting sfdx'
+  
+	def result = cdmSfdx("--version")
+	echo "${result}"
+}
+
 def cdmSfdx(String command, Boolean bypassError = false) {
     	def path = "\"${SFDX_HOME}\"" //adds '"' to the SFDX_HOME path in case there are spaces inside the path
 	
