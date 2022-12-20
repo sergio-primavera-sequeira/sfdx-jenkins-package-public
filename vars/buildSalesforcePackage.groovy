a9bb55883c7ff9b38b3fb3138f5ad90487e4ef68
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-20 11:56:41 -0500

    Update buildSalesforcePackage.groovy

--- vars/buildSalesforcePackage.groovy
+++ vars/buildSalesforcePackage.groovy
@@ -3,12 +3,21 @@ import java.util.Date
 import java.text.SimpleDateFormat
 
 def call(String DEV_HUB = 'none') {
-	configJob()
+	
+	def SFDC_ORG_01_JWT_KEY_CRED_ID="sf-jwt-key"
+        def SFDC_ORG_01_USER="integration.jenkins@sfjenkins.poc.org01.ca"
+        def SFDC_ORG_01="https://login.salesforce.com" 
+	def SFDC_ORG_01_CONNECTED_APP_CONSUMER_KEY="3MVG9ux34Ig8G5epoz.M1VfJxB82Qyj0J57NXfZmSeZWN5XytkVPTKSj7C9J.QYiwbdkPpmv9X0Efg0CKRXIX"
+	
+	configSFDX()
 }
 
-def configJob(){
+def configSFDX(){
+	
 	echo 'Starting sfdx'
-  
+	
+	env.SFDX_HOME = tool name: 'sfdx', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool' 
+	
 	def result = cdmSfdx("--version")
 	echo "${result}"
 }
