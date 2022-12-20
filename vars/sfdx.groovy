a43932d62622865229df7bb069f4516e34a3c03d
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-20 14:19:05 -0500

    Update sfdx.groovy

--- vars/sfdx.groovy
+++ vars/sfdx.groovy
@@ -5,7 +5,7 @@ def init(){
 	//SFDX HOME: an SFDX custom tool needs to be configured and the 'Tool Home' (when 'Install automatically' is checked) on custom tools needs to be configured
 	env.SFDX_HOME = tool name: 'sfdx', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool' 
 	
-	def result = command("--version")
+	def result = command("sfdx --version")
 	echo "${result}"
 }
 
@@ -17,7 +17,7 @@ def command(String command, Boolean bypassError = false) {
 	    if (isUnix()) {
 		return sh(returnStdout: true, script: "${path}/sfdx ${command}")
 	    } else {
-		return bat(returnStdout: true, script: "${path}/sfdx ${command}").trim()
+		return bat(returnStdout: true, script: "${path}/${command}").trim()
 	    }
 	} catch (Exception ex) {				
 		echo '==== SFDX ERROR ===='
