62fed927176f7c508a54265392ce0217f59e3443
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-20 17:31:14 -0500

    Update salesforceInstallPackage.groovy

--- vars/salesforceInstallPackage.groovy
+++ vars/salesforceInstallPackage.groovy
@@ -13,7 +13,14 @@ def call(String packageVersionId, String jwtCredentialId, String username, Strin
 			authenticateToDevHub(username, instanceUrl, consumerKey, jwt_key_file)
 			
 			echo "=== SFDX INSTALL PACKAGE ==="
-			installPackage(packageVersionId, username)
+			def packageInstallId = initiatePackageInstallation(packageVersionId, username)
+			echo 'Package Install ID :: ' + "${packageInstallId}"
+			
+			echo "=== SFDX INSTALL PACKAGE STATUS==="
+			def packageInstallStatus = getPackageInstallStatus(packageInstallId, username)
+			echo 'Package Install Status :: ' + "${packageInstallStatus}"
+			
+			return packageInstallStatus
 		}
 
 	} catch(Exception e) {
@@ -27,7 +34,7 @@ def authenticateToDevHub(String username, String instanceUrl, String connectedAp
 	echo "${result}"
 }
 
-def installPackage(String packageVersionId, String username){
+def initiatePackageInstallation(String packageVersionId, String username){
 	def result = sfdx.cmd("sfdx force:package:install --package ${packageVersionId} --wait 0 --apexcompile package --securitytype AdminsOnly --upgradetype Mixed --json --noprompt --targetusername ${username}")
 	result = result.readLines().drop(1).join(" ") //removes the first line of the output, for Windows only
 
@@ -54,7 +61,51 @@ def installPackage(String packageVersionId, String username){
 	echo 'Status :: ' + packageVersionInstallResultJson.result.Status
 	echo 'Errors :: ' + packageVersionInstallResultJson.result.Errors
 
-	def PACKAGE_VERSION_INSTALL_ID = packageVersionInstallResultJson.result.Id
-	echo 'PACKAGE_VERSION_INSTALL_ID :: ' + "${PACKAGE_VERSION_INSTALL_ID}"
-	return PACKAGE_VERSION_INSTALL_ID;
+	return packageVersionInstallResultJson.result.Id
+}
+
+def getPackageInstallStatus(String packageInstallId, String username){
+	def result
+	def packageVersionInstallResultJson
+	def currrentStatus
+	
+	while(true){
+
+		result = sfdx.cmd("sfdx force:package:install:report -i ${packageInstallId} --json --targetusername ${username}")
+		result = result.readLines().drop(1).join(" ") //removes the first line of the output, for Windows only
+		
+		packageVersionInstallResultJson = json.convertStringIntoJSON(result)
+		currrentStatus = packageVersionInstallResultJson.result.Status
+		
+		echo '======== LATEST PACKAGE INSTALL STATUS ========'
+		
+		echo 'status :: ' + packageVersionInstallResultJson.status
+		echo 'type :: ' + packageVersionInstallResultJson.result.type
+		echo 'url :: ' + packageVersionInstallResultJson.result.url
+		echo 'Id :: ' + packageVersionInstallResultJson.result.Id
+		echo 'IsDeleted :: ' + packageVersionInstallResultJson.result.IsDeleted
+		echo 'CreatedDate :: ' + packageVersionInstallResultJson.result.CreatedDate
+		echo 'CreatedById :: ' + packageVersionInstallResultJson.result.CreatedById
+		echo 'LastModifiedDate :: ' + packageVersionInstallResultJson.result.LastModifiedDate
+		echo 'LastModifiedById :: ' + packageVersionInstallResultJson.result.LastModifiedById
+		echo 'SystemModstamp :: ' + packageVersionInstallResultJson.result.SystemModstamp
+		echo 'SubscriberPackageVersionKey :: ' + packageVersionInstallResultJson.result.SubscriberPackageVersionKey
+		echo 'NameConflictResolution :: ' + packageVersionInstallResultJson.result.NameConflictResolution
+		echo 'PackageInstallSource :: ' + packageVersionInstallResultJson.result.PackageInstallSource
+		echo 'ProfileMappings :: ' + packageVersionInstallResultJson.result.ProfileMappings
+		echo 'Password :: ' + packageVersionInstallResultJson.result.Password
+		echo 'EnableRss :: ' + packageVersionInstallResultJson.result.EnableRss
+		echo 'UpgradeType :: ' + packageVersionInstallResultJson.result.UpgradeType
+		echo 'ApexCompileType :: ' + packageVersionInstallResultJson.result.ApexCompileType
+		echo 'Status :: ' + packageVersionInstallResultJson.result.Status
+		echo 'Errors :: ' + packageVersionInstallResultJson.result.Errors
+
+		if(currrentStatus.equalsIgnoreCase('Success') || currrentStatus.equalsIgnoreCase('Error')) {
+			break
+		}
+
+		sleep(time:10,unit:"SECONDS")
+	}
+	
+	return packageVersionInstallResultJson.result.Status
 }
