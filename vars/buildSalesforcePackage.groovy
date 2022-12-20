071039e09c46d68c2293ee53d7362ea6a7c7af55
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-20 10:41:54 -0500

    Create buildSalesforcePackage.groovy

---
+++ vars/buildSalesforcePackage.groovy
@@ -0,0 +1,26 @@
+def cdmSfdx(String command, Boolean bypassError = false) {
+    	def path = "\"${SFDX_HOME}\"" //adds '"' to the SFDX_HOME path in case there are spaces inside the path
+	
+	try {
+	    if (isUnix()) {
+		return sh(returnStdout: true, script: "${path}/sfdx ${command}")
+	    } else {
+		return bat(returnStdout: true, script: "${path}/sfdx ${command}").trim()
+	    }
+	} catch (Exception ex) {				
+		echo '==== SFDX ERROR ===='
+		echo ex.toString()
+		echo '===================='
+				
+		if(!bypassError){
+			throw ex
+		}
+		
+		return null
+	}	
+}
+
+def convertStringIntoJSON(String jsonStr) {
+    def json = readJSON text: jsonStr //need the Pipeline Utility Steps plugin
+    return json
+}
