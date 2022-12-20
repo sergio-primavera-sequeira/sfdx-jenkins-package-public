10adebd34b14085fd62c95567f532aed381681d7
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-20 14:25:28 -0500

    Update sfdx.groovy

--- vars/sfdx.groovy
+++ vars/sfdx.groovy
@@ -7,7 +7,7 @@ def init(){
 	//SFDX HOME: an SFDX custom tool needs to be configured and the 'Tool Home' (when 'Install automatically' is checked) on custom tools needs to be configured
 	env.SFDX_HOME = tool name: 'sfdx', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool' 
 	
-	def result = command("sfdx --version")
+	def result = this.command("sfdx --version")
 	echo "${result}"
 }
 
