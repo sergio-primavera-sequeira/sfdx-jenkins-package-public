9345c9485b02e4e46af25bfd7f8f6ec5497866df
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-20 12:07:25 -0500

    Update buildSalesforcePackage.groovy

--- vars/buildSalesforcePackage.groovy
+++ vars/buildSalesforcePackage.groovy
@@ -2,17 +2,17 @@
 import java.util.Date
 import java.text.SimpleDateFormat
 
-def call(String DEV_HUB = 'none') {
+def call(String dev_hub = 'none') {
 	
 	def SFDC_ORG_01_JWT_KEY_CRED_ID="sf-jwt-key"
         def SFDC_ORG_01_USER="integration.jenkins@sfjenkins.poc.org01.ca"
         def SFDC_ORG_01="https://login.salesforce.com" 
 	def SFDC_ORG_01_CONNECTED_APP_CONSUMER_KEY="3MVG9ux34Ig8G5epoz.M1VfJxB82Qyj0J57NXfZmSeZWN5XytkVPTKSj7C9J.QYiwbdkPpmv9X0Efg0CKRXIX"
 	
-	configSFDX()
+	configSfdx()
 }
 
-def configSFDX(){
+def configSfdx(){
 	
 	echo 'Starting sfdx'
 	
