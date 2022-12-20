47f8f2c08bf62a565768aa3871c9c54a634145c4
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-20 14:15:29 -0500

    Update buildSalesforcePackage.groovy

--- vars/buildSalesforcePackage.groovy
+++ vars/buildSalesforcePackage.groovy
@@ -9,7 +9,7 @@ def call(String dev_hub = 'none') {
         def SFDC_ORG_01="https://login.salesforce.com" 
 	def SFDC_ORG_01_CONNECTED_APP_CONSUMER_KEY="3MVG9ux34Ig8G5epoz.M1VfJxB82Qyj0J57NXfZmSeZWN5XytkVPTKSj7C9J.QYiwbdkPpmv9X0Efg0CKRXIX"
 	
-	configSfdx()
+	sfdx.init()
 	
 	try{
 		withCredentials([file(credentialsId: SFDC_ORG_01_JWT_KEY_CRED_ID, variable: 'jwt_key_file')]) {
@@ -20,40 +20,6 @@ def call(String dev_hub = 'none') {
 		currentBuild.result = 'FAILED'
 		throw e
 	}
-}
-
-def configSfdx(){
-	
-	echo 'Starting sfdx'
-	
-	//SFDX HOME: an SFDX custom tool needs to be configured and the 'Tool Home' (when 'Install automatically' is checked) on custom tools needs to be configured
-	env.SFDX_HOME = tool name: 'sfdx', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool' 
-	
-	def result = cdmSfdx("--version")
-	echo "${result}"
-}
-
-def cdmSfdx(String command, Boolean bypassError = false) {
-	
-    	def path = "\"${env.SFDX_HOME}\"" //adds '"' to the SFDX_HOME path in case there are spaces inside the path
-	
-	try {
-	    if (isUnix()) {
-		return sh(returnStdout: true, script: "${path}/sfdx ${command}")
-	    } else {
-		return bat(returnStdout: true, script: "${path}/sfdx ${command}").trim()
-	    }
-	} catch (Exception ex) {				
-		echo '==== SFDX ERROR ===='
-		echo ex.toString()
-		echo '===================='
-				
-		if(!bypassError){
-			throw ex
-		}
-		
-		return null
-	}	
 }
 
 def convertStringIntoJSON(String jsonStr) {
