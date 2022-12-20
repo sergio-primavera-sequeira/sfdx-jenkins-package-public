c44821a4a7af478b5b643fcdcd43c0fdb68ed4d7
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-20 15:02:04 -0500

    Update salesforcePackage.groovy

--- vars/salesforcePackage.groovy
+++ vars/salesforcePackage.groovy
@@ -2,24 +2,12 @@
 import java.util.Date
 import java.text.SimpleDateFormat
 
-def SFDC_ORG_01_JWT_KEY_CRED_ID
-def SFDC_ORG_01_USER
-def SFDC_ORG_01
-def SFDC_ORG_01_CONNECTED_APP_CONSUMER_KEY
-
-def call(String name = 'human') {
-	return "STSTST"
-}
-
-def init(String dev_hub = 'none') {
+def call(String dev_hub = 'none') {
 	
-	SFDC_ORG_01_JWT_KEY_CRED_ID="sf-jwt-key"
-	SFDC_ORG_01_USER="integration.jenkins@sfjenkins.poc.org01.ca"
-	SFDC_ORG_01="https://login.salesforce.com" 
-	SFDC_ORG_01_CONNECTED_APP_CONSUMER_KEY="3MVG9ux34Ig8G5epoz.M1VfJxB82Qyj0J57NXfZmSeZWN5XytkVPTKSj7C9J.QYiwbdkPpmv9X0Efg0CKRXIX"
-}
-
-def buildPackage(){
+	def SFDC_ORG_01_JWT_KEY_CRED_ID="sf-jwt-key"
+	def SFDC_ORG_01_USER="integration.jenkins@sfjenkins.poc.org01.ca"
+	def SFDC_ORG_01="https://login.salesforce.com" 
+	def SFDC_ORG_01_CONNECTED_APP_CONSUMER_KEY="3MVG9ux34Ig8G5epoz.M1VfJxB82Qyj0J57NXfZmSeZWN5XytkVPTKSj7C9J.QYiwbdkPpmv9X0Efg0CKRXIX"
 	
 	sfdx.init()
 	
