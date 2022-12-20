f4309078385e4f8546379ee3a00310dc8c645700
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-20 15:14:35 -0500

    Update buildSalesforcePackage.groovy

--- vars/buildSalesforcePackage.groovy
+++ vars/buildSalesforcePackage.groovy
@@ -13,10 +13,17 @@ def call(String dev_hub = 'none') {
 	
 	try{
 		withCredentials([file(credentialsId: DEV_HUB_ORG_JWT_KEY_CRED_ID, variable: 'jwt_key_file')]) {
-			return '!!!TEST SPS!!!'
+			
+			connectToDevHub(DEV_HUB_ORG_CONNECTED_APP_CONSUMER_KEY, DEV_HUB_ORG DEV_HUB_ORG_USER)
+			
 		}
 	} catch(Exception e) {
 		currentBuild.result = 'FAILED'
 		throw e
 	}
+}
+
+def connectToDevHub(String CONNECTED_APP_CONSUMER_KEY, String DEV_HUB_USER, String INSTANCE_URL){
+	def result = sfdx.cdm("sfdx force:auth:jwt:grant --clientid ${CONNECTED_APP_CONSUMER_KEY} --username ${DEV_HUB_USER} --setdefaultusername --jwtkeyfile ${jwt_key_file} --instanceurl ${INSTANCE_URL}")
+	echo "${result}"
 }
