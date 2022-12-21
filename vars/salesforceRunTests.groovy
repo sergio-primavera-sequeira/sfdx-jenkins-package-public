06de4b1b084ed0d3b3b976d2c30324ea9bcd7e5b
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-21 12:10:55 -0500

    Update salesforceRunTests.groovy

--- vars/salesforceRunTests.groovy
+++ vars/salesforceRunTests.groovy
@@ -2,7 +2,7 @@
 import java.util.Date
 import java.text.SimpleDateFormat
 
-def call(String subscriberPackageVersionId, String jwtCredentialId, String username, String instanceUrl, String consumerKey) {
+def call(String jwtCredentialId, String username, String instanceUrl, String consumerKey) {
 		
 	sfdx.init()
 
@@ -12,15 +12,10 @@ def call(String subscriberPackageVersionId, String jwtCredentialId, String usern
 			echo "=== SFDX AUTHENTICATION ==="
 			authenticateToDevHub(username, instanceUrl, consumerKey, jwt_key_file)
 			
-			echo "=== SFDX INSTALL PACKAGE ==="
-			def packageInstallId = initiatePackageInstallation(subscriberPackageVersionId, username)
-			echo 'Package Install ID :: ' + "${packageInstallId}"
-			
-			echo "=== SFDX INSTALL PACKAGE STATUS==="
-			def packageInstallStatus = getPackageInstallationStatus(packageInstallId, username)
-			echo 'Package Install Status :: ' + "${packageInstallStatus}"
-			
-			return packageInstallStatus
+			echo "=== SFDX RUN LOCAL TESTS ==="
+			def testResultsJson = initiatePackageInstallation(subscriberPackageVersionId, username)
+
+			return testResultsJson
 		}
 
 	} catch(Exception e) {
@@ -32,4 +27,22 @@ def call(String subscriberPackageVersionId, String jwtCredentialId, String usern
 def authenticateToDevHub(String username, String instanceUrl, String connectedAppConsumerkey, Object jwtKeyfile){
 	def result = sfdx.cmd("sfdx force:auth:jwt:grant --clientid ${connectedAppConsumerkey} --username ${username} --setdefaultusername --jwtkeyfile ${jwtKeyfile} --instanceurl ${instanceUrl}")
 	echo "${result}"
+}
+
+def runLocalTests(){
+	def result = sfdx.cmd("sfdx force:apex:test:run --testlevel RunLocalTests --synchronous --resultformat json --detailedcoverage  --codecoverage")
+	result = result.readLines().drop(1).join(" ") //removes the first line of the output, for Windows only
+
+	def testResultJson = json.convertStringIntoJSON(result)
+
+	echo 'Outcome :: ' + testResultJson.result.summary.outcome
+	echo 'Tests Ran :: ' + testResultJson.result.summary.testsRan
+	echo 'Passing :: ' + testResultJson.result.summary.passing
+	echo 'Failing :: ' + testResultJson.result.summary.failing
+	echo 'Pass Rate :: ' + testResultJson.result.summary.passRate
+	echo 'Fail Rate :: ' + testResultJson.result.summary.failRate
+	echo 'Test Run Coverage :: ' + testResultJson.result.summary.testRunCoverage
+	echo 'Org Wide Coverage :: ' + testResultJson.result.summary.orgWideCoverage
+	
+	return testResultJson
 }
