5d56cf62dc3d7b7a4b8201a37c9a491d23c13c21
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-21 12:00:31 -0500

    Update salesforceRunTests.groovy

--- vars/salesforceRunTests.groovy
+++ vars/salesforceRunTests.groovy
@@ -1,1 +1,35 @@
+#!/usr/bin/env groovy
+import java.util.Date
+import java.text.SimpleDateFormat
 
+def call(String subscriberPackageVersionId, String jwtCredentialId, String username, String instanceUrl, String consumerKey) {
+		
+	sfdx.init()
+
+	try{
+		withCredentials([file(credentialsId: jwtCredentialId, variable: 'jwt_key_file')]) {
+
+			echo "=== SFDX AUTHENTICATION ==="
+			authenticateToDevHub(username, instanceUrl, consumerKey, jwt_key_file)
+			
+			echo "=== SFDX INSTALL PACKAGE ==="
+			def packageInstallId = initiatePackageInstallation(subscriberPackageVersionId, username)
+			echo 'Package Install ID :: ' + "${packageInstallId}"
+			
+			echo "=== SFDX INSTALL PACKAGE STATUS==="
+			def packageInstallStatus = getPackageInstallationStatus(packageInstallId, username)
+			echo 'Package Install Status :: ' + "${packageInstallStatus}"
+			
+			return packageInstallStatus
+		}
+
+	} catch(Exception e) {
+		currentBuild.result = 'FAILED'
+		throw e
+    }
+}
+
+def authenticateToDevHub(String username, String instanceUrl, String connectedAppConsumerkey, Object jwtKeyfile){
+	def result = sfdx.cmd("sfdx force:auth:jwt:grant --clientid ${connectedAppConsumerkey} --username ${username} --setdefaultusername --jwtkeyfile ${jwtKeyfile} --instanceurl ${instanceUrl}")
+	echo "${result}"
+}
