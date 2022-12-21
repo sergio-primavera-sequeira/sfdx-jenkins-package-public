69a78435e94a33aa5e6b9cb4314b3c5f9b70663e
Sergio Primavera Sequeira <48224451+sergio-primavera-sequeira@users.noreply.github.com>
2022-12-21 12:31:07 -0500

    Update salesforcePromotePackage.groovy

--- vars/salesforcePromotePackage.groovy
+++ vars/salesforcePromotePackage.groovy
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat
 * promotes a package from beta to a release ready
 * only one <major.minor.patch> version of a package can be promoted
 */
-def call(String subscriberPackageVersionId, String jwtCredentialId, String devHubUsername, String devHubInstanceUrl, String devHubConsumerKey) {
+def call(String subscriberPackageVersionId, String jwtCredentialId, String devHubUsername, String devHubInstanceUrl, String devHubConsumerKey, Boolean bypassError) {
 		
 	sfdx.init()
 	
@@ -14,10 +14,10 @@ def call(String subscriberPackageVersionId, String jwtCredentialId, String devHu
 		withCredentials([file(credentialsId: jwtCredentialId, variable: 'jwt_key_file')]) {
 			
 			echo "=== SFDX AUTHENTICATION ==="
-			authenticateToDevHub(devHubUsername, devHubInstanceUrl, devHubConsumerKey, jwt_key_file)
+			authenticateSalesforceOrg(devHubUsername, devHubInstanceUrl, devHubConsumerKey, jwt_key_file)
 			
 			echo "=== SFDX PROMOTE PACKAGE VERSION ==="
-			promotePackageVersion(subscriberPackageVersionId, devHubUsername)
+			promotePackageVersion(subscriberPackageVersionId, devHubUsername, bypassError)
 		}
 		
 	} catch(Exception e) {
@@ -26,18 +26,28 @@ def call(String subscriberPackageVersionId, String jwtCredentialId, String devHu
 	}
 }
 
-def authenticateToDevHub(String username, String instanceUrl, String connectedAppConsumerkey, Object jwtKeyfile){
+def authenticateSalesforceOrg(String username, String instanceUrl, String connectedAppConsumerkey, Object jwtKeyfile){
 	def result = sfdx.cmd("sfdx force:auth:jwt:grant --clientid ${connectedAppConsumerkey} --username ${username} --setdefaultusername --jwtkeyfile ${jwtKeyfile} --instanceurl ${instanceUrl}")
 	echo "${result}"
 }
 
-def promotePackageVersion(String subscriberPackageVersionId, String devHubUsername){
-	def result = sfdx.cmd("sfdx force:package:version:promote --package ${subscriberPackageVersionId} --json --noprompt --targetdevhubusername ${devHubUsername}", true)
-
+def promotePackageVersion(String subscriberPackageVersionId, String devHubUsername, Boolean bypassError){
+	def result = sfdx.cmd("sfdx force:package:version:promote --package ${subscriberPackageVersionId} --json --noprompt --targetdevhubusername ${devHubUsername}", bypassError)
+	
 	if(result != null) {
-		echo "${result}"
+		result = result.readLines().drop(1).join(" ") //removes the first line of the output, for Windows only
+		def resultJson = json.convertStringIntoJSON(result)
+		
+		echo 'status :: ' + resultJson.status
+		echo 'Id :: ' + resultJson.result.id
+		echo 'success :: ' + resultJson.result.success
+		echo 'errors :: ' + resultJson.result.errors
+		
+		return resultJson
+		
 	} else {
 		echo 'Skipped the Package Promotion Stage due to an SFDX error...'
+		return null
 	}
 }
 
